4ec35edde7faded5c14d0221e668717c6430c367
TiffanyZTH <tiffany.zhangtianhui@gmail.com>
2023-03-14 22:43:20 -0400

    Modified the implementation

--- day1/src/com/company/SongCacheImpl.java
+++ day1/src/com/company/SongCacheImpl.java
@@ -2,6 +2,10 @@ package com.company;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -26,40 +30,47 @@ interface SongCache {
 }
 public class SongCacheImpl implements SongCache{
     // Use ConcurrentHashMap to ensure the thread synchronized and efficient
-    private ConcurrentHashMap<String, Integer> map;
+    private final ConcurrentHashMap<String, Integer> songs;
     public SongCacheImpl(){
-        map = new ConcurrentHashMap<>();
+        songs = new ConcurrentHashMap<>();
     }
     @Override
     public void recordSongPlays(String songId, int numPlays) {
         // update the new value of given ID
-        map.put(songId, map.getOrDefault(songId, 0)+numPlays);
+        if (songs.containsKey(songId)){
+            songs.compute(songId, (key, value)-> value + numPlays);
+        }else{
+            songs.put(songId, numPlays);
+        }
     }
 
     @Override
     public int getPlaysForSong(String songId) {
-        return map.getOrDefault(songId, -1);
+        return songs.getOrDefault(songId, -1);
     }
 
     @Override
     public List<String> getTopNSongsPlayed(int n) {
         if (n<=0) return null;
         // if the input is bigger than the total number of songs, return all
-        if (n>map.size()) return new ArrayList<>(map.keySet());
+        if (n>songs.size()) return new ArrayList<>(songs.entrySet().stream()
+                .sorted((e1,e2)->e2.getValue().compareTo(e1.getValue()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList()));
 
-        List<String> res = new ArrayList<>();
-        // Add your map elements into a list which accepts EntrySet<String, Integer> as Input
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-        // Sort the list by using comparingByValue method in Entry
-        list.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
-        // Add the keys to result list
-        for(Map.Entry<String, Integer> e :list.subList(0,n)){
-            res.add(e.getKey());
+        // Sort the nodes by using PriorityQueue (minimum heap)
+        PriorityQueue<Map.Entry<String,Integer>> heap = new PriorityQueue<>(n, Map.Entry.comparingByValue());
+        for (Map.Entry<String, Integer> entry : songs.entrySet()) {
+            heap.add(entry);
+            // if the size is beyond required number, poll 1 minimum node
+            if (heap.size() > n) {
+                heap.poll();
+            }
         }
-        return res;
+        return heap.stream().map(Map.Entry::getKey).collect(Collectors.toList());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // write your code here
         SongCache cache = new SongCacheImpl();
         cache.recordSongPlays("ID-1", 3);
@@ -68,7 +79,28 @@ public class SongCacheImpl implements SongCache{
         cache.recordSongPlays("ID-3", 5);
         assertThat(cache.getPlaysForSong("ID-1"), is(4));
         assertThat(cache.getPlaysForSong("ID-9"), is(-1));
-        assert(cache.getTopNSongsPlayed(2).contains("ID-3")&&cache.getTopNSongsPlayed(2).contains("ID-1")==true);
+        System.out.println(cache.getTopNSongsPlayed(8));
+        assert(cache.getTopNSongsPlayed(2).contains("ID-3")&&
+                cache.getTopNSongsPlayed(2).contains("ID-1")==true);
         assert(cache.getTopNSongsPlayed(0)==null);
+
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 3; i++) {
+            threadPool.execute(new Thread(()->{
+                cache.recordSongPlays("ID-3", 5);
+                System.out.println();
+            },"Thread"+i));
+        }
+        //TODO: IllegalMonitorStateException
+        try{
+            System.out.println(threadPool.isTerminated());
+            while (!threadPool.isTerminated()){
+                threadPool.wait(1000);
+                System.out.println(threadPool.shutdownNow());
+            }
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        assertThat(cache.getPlaysForSong("ID-3"), is(20));
     }
 }
